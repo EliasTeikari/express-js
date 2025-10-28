5bfc4d327e851d17f076647d1ec9d9633f25ccc0
Elias Teikari <elias.tkri@gmail.com>
2025-10-29 00:47:40 +0200

    Express endpoints tested with postman

--- app.js
+++ app.js
@@ -1,17 +1,47 @@
-const http = require("http");
+const express = require("express");
+const app = express();
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.write("Hello World");
-    res.end();
-  }
+app.use(express.json());
 
-  if (req.url === "/api/courses") {
-    res.write(JSON.stringify([1, 2, 3]));
-    res.end();
-  }
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
+
+app.get("/", (req, res) => {
+  res.send("Hello world!!!");
 });
 
-server.listen(3000);
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
+});
 
-console.log("Listening on port 3000...");
+app.post("/api/courses", (req, res) => {
+  if (!req.body.name || req.body.name.length < 3) {
+    // 400 Bad Request
+    res.status(400).send("Name is required and should be minimum 3 characters");
+    return;
+  }
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) {
+    res.status(404).send("The course with the given ID was not found.");
+  }
+  res.send(course);
+});
+
+// PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
